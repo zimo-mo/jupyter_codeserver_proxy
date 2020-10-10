a5613a874823994e2ca01f52d0647e60cabdf104
weiqiang.han <weiqiang.han@msxf.com>
2020-10-10 15:27:27 +0800

    add extension dir

--- jupyter_codeserver_proxy/__init__.py
+++ jupyter_codeserver_proxy/__init__.py
@@ -7,6 +7,7 @@ for more information.
 import os
 import shutil
 
+
 def setup_codeserver():
     # Make sure codeserver is in $PATH
     def _codeserver_command(port):
@@ -16,9 +17,16 @@ def setup_codeserver():
         working_dir = os.getenv("CODE_WORKINGDIR", None)
         if working_dir is None:
             working_dir = os.getenv("JUPYTER_SERVER_ROOT", ".")
-
-        return [full_path, "--bind-addr", f"0.0.0.0:{port}",
-                "--auth", "none", "--show-versions", working_dir]
+        extensions_dir = os.path.expanduser("~/.vscode/extensions")
+        command = [
+            full_path, "--bind-addr", f"0.0.0.0:{port}", "--auth", "none",
+            "--disable-telemetry", "--show-versions"
+        ]
+        if os.path.exists(extensions_dir):
+            command.append("--extensions-dir")
+            command.append(extensions_dir)
+        command.append(working_dir)
+        return command
 
     return {
         'command': _codeserver_command,
@@ -26,9 +34,10 @@ def setup_codeserver():
         'absolute_url': False,
         'new_browser_tab': False,
         'launcher_entry': {
-            'title': 'VS Code IDE',
-            'icon_path': os.path.join(os.path.dirname(
-                                            os.path.abspath(__file__)),
-                                      'icons', 'vscode.svg')
+            'title':
+            'VS Code IDE',
+            'icon_path':
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons',
+                         'vscode.svg')
         }
     }
